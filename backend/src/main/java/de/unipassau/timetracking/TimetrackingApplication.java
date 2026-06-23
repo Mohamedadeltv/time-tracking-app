7f2e2a673a145d14ba3a36cec4098c8a2363ad29
Mohamed Abouhitta <abouhi01@ads.uni-passau.de>
2026-06-23 03:48:07 +0200

    Add GitHub Actions CI with backend Jacoco/Spotless and frontend lint/test/coverage (#7)

--- backend/src/main/java/de/unipassau/timetracking/TimetrackingApplication.java
+++ backend/src/main/java/de/unipassau/timetracking/TimetrackingApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TimetrackingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TimetrackingApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TimetrackingApplication.class, args);
+  }
 }
